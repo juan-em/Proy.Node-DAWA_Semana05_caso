9b1c295a33084c7b0e8ae1ed18975270b09f72b9
juan-em <9jescobarm6@gmail.com>
2023-08-11 12:18:51 -0500

    commit@

---
+++ index.js
@@ -0,0 +1,8 @@
+const {Server} = require('socket.io')
+const io = new Server(server)
+
+const express = require('express')
+const app = express()
+
+const http = require('http')
+const server = http.createServer(app)
